6555dbf910ed7a6dd645b8eacc9b69e91240a541
guilo84 <95195768+guilo84@users.noreply.github.com>
2025-02-14 16:36:43 -0500

    Update ModuloTest.py
    cleaned up number addition function

--- ModuloTest.py
+++ ModuloTest.py
@@ -1,15 +1,16 @@
 import inspect
+import math
 
 
+def digitsum(n):
+    while n > 9:
+        n = sum(int(digit) for digit in str(n))
+    print(n)
 
-integer = int(input("What is the integer? "))
-divisor = int(input("What is the divisor? "))
-quotient = str(integer % divisor)
 
-print(quotient)
-
-number_str = quotient
-sum_of_digits = 0
-for digit in number_str:
-    sum_of_digits += int(digit)
-print(sum_of_digits)
+# integer = int(input("What is the integer? "))
+# divisor = int(input("What is the divisor? "))
+# quotient = str(integer % divisor)
+#
+# print(quotient)
+digitsum(19)
