79c7f4bd98a849ce8847b114acb414d0f3291ac0
guilo84 <95195768+guilo84@users.noreply.github.com>
2025-02-15 13:15:37 -0500

    Update ModuloTest.py
    Fixed digitsum function to not return none
    fixed operations and choose integers functions to work

--- ModuloTest.py
+++ ModuloTest.py
@@ -5,33 +5,32 @@ import math
 def digitsum(n):
     while n > 9:
         n = sum(int(digit) for digit in str(n))
-    print(n)
+    return n
 
-def chooseintegers():
-    chooseintegers.integer_one = int(input("What is the first integer? "))
-    chooseintegers.integer_two = int(input("What is the second integer? "))
-    one = chooseintegers.integer_one
-    two = chooseintegers.integer_two
-    return one, two
+def choose_integers():
+    choose_integers.integer_one = int(input("What is the first integer? "))
+    choose_integers.integer_two = int(input("What is the second integer? "))
+    # one = choose_integers.integer_one
+    # two = choose_integers.integer_two
+    return choose_integers.integer_one, choose_integers.integer_two
 
 def operation(int_one,int_two):
-    input("Do you want to use 1.) addition, 2.) multiplication, or 3.) exponent? ")
-    answer = 0
-    if input == 1:
-        answer = int_one + int_two
-        return answer
-    elif input == 2:
-        answer = int_one * int_two
-        return answer
-    elif input == 3:
-        answer = int_one ^ int_two
-        return answer
-    return answer
-chooseintegers()
-int_one = chooseintegers.integer_one
-int_two = chooseintegers.integer_two
+    choice = input("Do you want to add, multiply, or exponent? ")
+    if choice == "add":
+        return int_one + int_two
+    elif choice == "multiply":
+        return int_one * int_two
+    elif choice == "exponent":
+        return int_one ** int_two
+    else:
+        print("No input")
+
+
+choose_integers()
+int_one = choose_integers.integer_one
+int_two = choose_integers.integer_two
 print(int_one)
 print(int_two)
-operation(int_one, int_two)
-result = operation.answer
-# digitsum(1948468113113186846)
+result = operation(int_one, int_two)
+print(result)
+print(digitsum(result))
